296708cb6f2acc2984f1b4a62b30d6f765396c99
Artxmvl <talakhovartem2007@gmail.com>
2025-12-15 13:49:43 +0300

    Добавлена функция деления

--- calculator.cpp
+++ calculator.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <stdexcept>
 
 int add(int a, int b) {
     return a + b;
+}
+
+double divide(int a, int b) {
+    if (b == 0) {
+        throw std::runtime_error("Деление на ноль невозможно");
+    }
+    return static_cast<double>(a) / b;
 }
 
 int main() {
     std::cout << add(2, 3) << std::endl;
     return 0;
 }
+
+
